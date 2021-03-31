66ed76744f1c2ab8efb84403a978b19b3488027a
Stepmnvk <stepmnvk@ukr.net>
2021-03-31 14:03:26 +0300

    new_statut

--- assets/img/statut/resize-photo.sh
+++
@@ -1,6 +0,0 @@
-p=$(pwd);
-for img in *.jpg;
-do let j+=1;
-convert -resize 1920 -quality 75  $img $p\/$img;
-done 
-
--- assets/img/statut/smallPhoto.sh
+++
@@ -1,8 +0,0 @@
-j=0;
-p=$(pwd);
-mkdir $p\/small
-for img in *.jpg;
-do let j+=1;
-convert -resize 300 -quality 75  $img $p\/small\/$img;
-done 
-
